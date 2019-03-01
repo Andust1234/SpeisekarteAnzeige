1108ed6b016f9b2e09ccda9d1eacbe100ac24c87
Zombiecat92 <timo.mueller92@oulook.de>
2019-03-01 09:03:46 +0100

    Right Dir

---
+++ speisekarte_unity/SpeiseKarte IT-Sys/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour
+{
+    public MySQLController other;
+    public Text input;
+
+    public void Insert()
+    {
+        string sqlText = "INSERT INTO test (name) VALUE ('" + input.text + "')";
+
+        other.InsertDB(sqlText);
+    }
+}
---
+++ speisekarte_unity/SpeiseKarte IT-Sys/Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("MySQLController");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
